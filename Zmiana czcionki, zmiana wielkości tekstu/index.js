257bccb51b8c5188da654eca62a8d5fad26d1144
Michał <misiuk92@gmail.com>
2025-03-03 12:37:21 +0100

    Add files via upload

---
+++ Zmiana czcionki, zmiana wielkości tekstu/index.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const rangeInput = document.querySelector('.rangeInput');
+  const sampleText = document.querySelector('.sampleText');
+  const fontSelector = document.querySelector('.fontSelector');
+
+  rangeInput.addEventListener('input', function () {
+    sampleText.style.fontSize = this.value + 'px';
+  });
+
+  fontSelector.addEventListener('change', () => {
+    sampleText.style.fontFamily = fontSelector.value;
+  });
+});
